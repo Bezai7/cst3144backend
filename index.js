c3570041dd6640eef808b91a503419924030be86
Bezai7 <bezaiadje0@gmail.com>
2025-12-07 22:57:43 +0400

    Add Morgan logger and custom logger middleware

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 var express = require("express");
-var cors = require("cors");  // NEW
-var bodyParser = require("body-parser");  // NEW
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var morgan = require("morgan");  // NEW
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(cors());  // NEW
-app.use(bodyParser.json());  // NEW
-app.set('json spaces', 3);  // NEW
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('dev'));  // NEW
+app.set('json spaces', 3);
+
+// NEW
+app.use((req, res, next) => {
+  console.log("In comes a request to: " + req.url);
+  next();
+});
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
