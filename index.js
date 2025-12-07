3b3b400a5f4a699cba3365d171006f1f41e851ca
Bezai7 <bezaiadje0@gmail.com>
2025-12-07 22:53:55 +0400

    Add CORS and body parser middleware

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require("express");
+var cors = require("cors");  // NEW
+var bodyParser = require("body-parser");  // NEW
+
 var app = express();
 var port = process.env.PORT || 3000;
+
+app.use(cors());  // NEW
+app.use(bodyParser.json());  // NEW
+app.set('json spaces', 3);  // NEW
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
