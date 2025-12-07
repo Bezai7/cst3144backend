54b3a4c702aa4427893de7e596499541abe325d1
Bezai7 <bezaiadje0@gmail.com>
2025-12-07 23:00:42 +0400

    Set up MongoDB connection with environment variables

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
-var morgan = require("morgan");  // NEW
+var morgan = require("morgan");
+var { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");  // NEW
+require('dotenv').config();  // NEW
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+// NEW - MongoDB connection setup
+var uri = process.env.MONGODB_URI;
+if (!uri) {
+  console.error('MONGODB_URI not set');
+  process.exit(1);
+}
+
+var client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+
+var db;
+var lessonsCollection;
+var ordersCollection;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    db = client.db(process.env.DB_NAME || 'lavenderacademy');
+    lessonsCollection = db.collection('lesson');
+    ordersCollection = db.collection('order');
+    console.log('Collections ready');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+}
+
+connectDB();
+// END NEW
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan('dev'));  // NEW
+app.use(morgan('dev'));
 app.set('json spaces', 3);
 
-// NEW
 app.use((req, res, next) => {
   console.log("In comes a request to: " + req.url);
   next();
